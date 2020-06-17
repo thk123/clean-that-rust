f36feae12017807eefe4ba3d58b94759c96e41f5
Thomas Kiley <thomas.kiley@diffblue.com>
2020-06-17 11:57:56 +0100

    Move store code into separate file

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,8 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-struct DirtyArea
-{
-    area_name : String,
-    dirtieness_score : u32,
-}
+use crate::store::store::Store;
 
-struct StoreTK
-{
-    scores : std::collections::HashMap<String, DirtyArea>,
-}
+mod store;
 
-impl StoreTK
-{
-    fn declare_area(&mut self, area_name : &str)
-    {
-        let new_area = DirtyArea {
-            area_name: String::from(area_name),
-            dirtieness_score: 0,
-        };
-        self.scores.insert(String::from(area_name), new_area);
-    }
-    fn score_of(&self, area_name : &str) -> std::option::Option<u32>
-    {
-        match self.scores.get(&String::from(area_name))
-        {
-            None => {None},
-            Some(area) => {Some(area.dirtieness_score)},
-        }
-    }
-
-}
-
-#[cfg(test)]
-mod store_tests {
-    use crate::StoreTK;
-
-    #[test]
-    fn add_an_area()
-    {
-        let mut store = StoreTK
-        {
-            scores:std::collections::HashMap::new()
-        };
-        store.declare_area("bathroom sink");
-        assert_eq!(store.score_of("bathroom sink").expect("Room not found"), 0);
-    }
-}
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -56,6 +13,9 @@ async fn index2() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    let mut store = Store::initialize();
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
---
+++ src/store.rs
@@ -0,0 +1,51 @@
+pub mod store
+{
+    pub struct DirtyArea
+    {
+        area_name: String,
+        dirtieness_score: u32,
+    }
+
+    pub struct Store
+    {
+        scores: std::collections::HashMap<String, DirtyArea>,
+    }
+
+    impl Store
+    {
+        pub fn declare_area(&mut self, area_name: &str)
+        {
+            let new_area = DirtyArea {
+                area_name: String::from(area_name),
+                dirtieness_score: 0,
+            };
+            self.scores.insert(String::from(area_name), new_area);
+        }
+        pub fn score_of(&self, area_name: &str) -> std::option::Option<u32>
+        {
+            match self.scores.get(&String::from(area_name))
+            {
+                None => { None },
+                Some(area) => { Some(area.dirtieness_score) },
+            }
+        }
+
+        pub fn initialize() -> Store
+        {
+            Store { scores: std::collections::HashMap::new() }
+        }
+    }
+
+    #[cfg(test)]
+    mod store_tests {
+        use crate::Store;
+
+        #[test]
+        fn add_an_area()
+        {
+            let mut store = Store::initialize();
+            store.declare_area("bathroom sink");
+            assert_eq!(store.score_of("bathroom sink").expect("Room not found"), 0);
+        }
+    }
+}
