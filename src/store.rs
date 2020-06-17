2f6654a34e54c22f6c33b1c61008c5401433d634
Thomas Kiley <thomas.kiley@diffblue.com>
2020-06-17 15:50:01 +0100

    Swap store to use database

--- src/store.rs
+++ src/store.rs
@@ -1,72 +1,90 @@
 pub mod store
 {
     use std::cmp::max;
+    use self::unqlite::{UnQLite, KV, Error};
+    use std::convert::TryInto;
 
-    pub struct DirtyArea
-    {
-        area_name: String,
-        dirtieness_score: u32,
-    }
+    extern crate unqlite;
 
     pub struct Store
     {
-        scores: std::collections::HashMap<String, DirtyArea>,
+        // scores: std::collections::HashMap<String, DirtyArea>,
+        database : UnQLite,
+    }
+
+    fn convert_to(dirtiness : u32) -> [u8; 4]
+    {
+        dirtiness.to_le_bytes()
+    }
+
+    fn convert_from(stored_data : &Vec<u8>) -> u32
+    {
+        if stored_data.len() != 4
+        {
+            panic!("Database corrupted");
+        }
+        let (int_bytes, _) = stored_data.split_at(std::mem::size_of::<u32>());
+        u32::from_le_bytes(int_bytes.try_into().unwrap())
     }
 
     impl Store
     {
-        pub fn declare_area(&mut self, area_name: &str) -> Result<String, String>
+        pub fn declare_area(&self, area_name: &str) -> Result<String, String>
         {
-            if self.scores.contains_key(&String::from(area_name))
+            if self.database.kv_contains(&String::from(area_name))
             {
                 return Err("Area already exists ".to_owned() + area_name);
             }
 
-            let new_area = DirtyArea {
-                area_name: String::from(area_name),
-                dirtieness_score: 0,
-            };
-            self.scores.insert(String::from(area_name), new_area);
+            self.database.kv_store(&String::from(area_name), convert_to(0));
             Ok(String::from(area_name))
         }
         pub fn score_of(&self, area_name: &str) -> std::option::Option<u32>
         {
-            match self.scores.get(&String::from(area_name))
+            match self.database.kv_fetch(area_name)
             {
-                None => { None }
-                Some(area) => { Some(area.dirtieness_score) }
+                Ok(score) => {
+                    Some(convert_from(&score))
+                },
+                Err(_) => { None },
             }
         }
 
-        pub fn clean_area(&mut self, area_name: &str) -> Result<String, String>
+        pub fn clean_area(&self, area_name: &str) -> Result<String, String>
         {
-            match self.scores.get_mut(&String::from(area_name))
+            if !self.database.kv_contains(&String::from(area_name))
             {
-                None => { Err("Could not find area: ".to_owned() + area_name) }
-                Some(area) => {
-                    area.dirtieness_score = 0;
-                    Ok(String::from(area_name))
-                }
+                return Err("Area does not exist: ".to_owned() + area_name);
             }
+
+            self.database.kv_store(&String::from(area_name), convert_to(0));
+            return Ok(String::from(area_name));
         }
 
-        pub fn adjust_score(&mut self, area_name: &str, increment_size: i32) -> Result<u32, String>
+        pub fn adjust_score(&self, area_name: &str, increment_size: i32) -> Result<u32, String>
         {
-            match self.scores.get_mut(&String::from(area_name))
+            let current_score = self.score_of(area_name);
+            match current_score
             {
-                None => { Err("Could not find ".to_owned() + area_name) }
-                Some(area) =>
-                    {
-                        let new_score = area.dirtieness_score as i32 + increment_size;
-                        area.dirtieness_score = max(new_score, 0) as u32;
-                        Ok(area.dirtieness_score)
-                    }
+                None => { Err("Could not find ".to_owned() + area_name) },
+                Some(current_score) => {
+                    let new_score = max(current_score as i32 + increment_size, 0) as u32;
+                    self.database.kv_store(area_name, convert_to(new_score));
+                    Ok(new_score)
+                },
             }
         }
 
         pub fn initialize() -> Store
         {
-            Store { scores: std::collections::HashMap::new() }
+            Store { database: UnQLite::create_temp() }
+        }
+
+        pub fn initialize_from(database_str : &str) -> Store
+        {
+            Store {
+                database : UnQLite::create(database_str),
+            }
         }
     }
 
